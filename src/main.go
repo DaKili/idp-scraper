84bc9761485740f4c83b041410520d01c8168824
yrumad <yrumad@machine.localdomain>
2023-05-28 12:18:57 +0200

    add todos

--- src/main.go
+++ src/main.go
@@ -1,7 +1,8 @@
 package main
 
 func main() {
-
+	// Todo: make scanner only browse pages, if no old idp is found on the current page
+	// Todo: bug trying to upload an empty dictionary
 	projects := make(map[string]Project)
 	existingProjects := getProjects()
 	ScanERI(&projects, &existingProjects)
